67dbf0a3f996861277ccdb3b3996839cbfe6444a
Alexander Brauckmann <a.brauckmann@gmail.com>
2023-01-13 17:13:12 +0000

    Init

--- irSynth/enumeration/CandidateTuples.cc
+++ irSynth/enumeration/CandidateTuples.cc
@@ -15,6 +15,94 @@ using namespace mlir;
 // arguments that need to be known at compile-time. Therefore, specialized
 // functions are created for some sizes.
 
+template <int n_op, int n_attr, int n_region>
+auto cartesianProduct(std::vector<CandidatePtr> &operandCandidates,
+                       std::vector<Attribute> &attributeCandidates,
+                       std::vector<std::shared_ptr<Region>> &regionCandidates) -> std::vector<CandidateTuple>;
+
+template<int n_op, int n_attr, int n_region>
+struct CartesianProduct;
+
+template<>
+struct CartesianProduct<0, 0, 0> {
+  template<class ... Args>
+  static auto apply(std::vector<CandidatePtr> &/*operandCandidates*/,
+                    std::vector<Attribute> &/*attributeCandidates*/,
+                    std::vector<std::shared_ptr<Region>> &/*regionCandidates*/,
+                    Args && ... args) -> std::vector<CandidateTuple> {
+    return ranges::views::cartesian_product(std::forward<Args>(args)...);
+  }
+};
+
+
+
+template<int n_region>
+struct CartesianProduct<0, 0, n_region> {
+  template<class ... Args>
+  static auto apply(std::vector<CandidatePtr> &operandCandidates,
+                    std::vector<Attribute> &attributeCandidates,
+                    std::vector<std::shared_ptr<Region>> &regionCandidates,
+                    Args && ... args) -> std::vector<CandidateTuple> {
+  return CartesianProduct<0, 0, n_region - 1>::apply(operandCandidates, attributeCandidates, regionCandidates, std::forward<Args>(args)..., regionCandidates);
+}
+};
+
+
+template<int n_attr, int n_region>
+struct CartesianProduct<0, n_attr, n_region> {
+  template<class ... Args>
+  static auto apply(std::vector<CandidatePtr> &operandCandidates,
+                    std::vector<Attribute> &attributeCandidates,
+                    std::vector<std::shared_ptr<Region>> &regionCandidates,
+                    Args && ... args) -> std::vector<CandidateTuple> {
+  return CartesianProduct<0, n_attr - 1, n_region>::apply(operandCandidates, attributeCandidates, regionCandidates, std::forward<Args>(args)..., attributeCandidates);
+}
+};
+
+
+template<int n_op, int n_attr, int n_region>
+struct CartesianProduct {
+  template<class ... Args>
+  static auto apply(std::vector<CandidatePtr> &operandCandidates,
+                    std::vector<Attribute> &attributeCandidates,
+                    std::vector<std::shared_ptr<Region>> &regionCandidates,
+                    Args && ... args) -> std::vector<CandidateTuple> {
+  return CartesianProduct<n_op - 1, n_attr, n_region>::apply(operandCandidates, attributeCandidates, regionCandidates, std::forward<Args>(args)..., operandCandidates);
+}
+};
+
+template<class U, class U2, int start, class T, T ... ints>
+auto createTupleImpl(U2 & cand, std::integer_sequence<T, ints...> int_seq) -> U2 {
+  return {std::get<start + ints>(cand)...};
+}
+
+template<class T, class U2, int start, std::size_t N, typename Indices = std::make_index_sequence<N>>
+auto createTuple(U2 & cand) -> T {
+  return createTupleImpl<start>(cand, Indices{});
+}
+
+template <int n_op, int n_attr, int n_region>
+std::vector<CandidateTuple>
+getCartesianProduct(std::vector<CandidatePtr> &operandCandidates,
+                       std::vector<Attribute> &attributeCandidates,
+                       std::vector<std::shared_ptr<Region>> &regionCandidates) {
+  auto cands = CartesianProduct<n_op, n_attr, n_region>::apply(operandCandidates, attributeCandidates, regionCandidates);
+  std::vector<CandidateTuple> ret;
+  for (auto cand : cands) {
+    CandidateTuple tuple;
+    //tuple.operands = {std::get<0>(cand)};
+    tuple.operands = createTuple<decltype(tuple.operands), 0, n_op>(cand);
+    if constexpr (n_attr > 0) {
+      tuple.attributes = createTuple<decltype(tuple.attributes), n_op, n_attr>(cand);
+    }
+    if constexpr (n_region > 0) {
+      tuple.regions = createTuple<decltype(tuple.regions), n_op + n_attr, n_region>(cand);
+    }
+    ret.push_back(tuple);
+  }
+  return ret;
+}
+
 std::vector<CandidateTuple>
 getCartesianProduct110(std::vector<CandidatePtr> &operandCandidates,
                        std::vector<Attribute> &attributeCandidates,
@@ -98,6 +186,17 @@ getCartesianProduct210(std::vector<CandidatePtr> &operandCandidates,
   return ret;
 }
 
+//static constexpr std::array<std::array>>> foo = createArray(2,2,2);
+//constexpr auto createArray(int x, int y, int z) {
+//  for (int i = 0; i < x; i++) {
+//    for (int j = 0; j < y; j++) {
+//      for (int k = 0; k < z; k++) {
+//        foo[i,j,k] = getCartesianProduct<i,j,k>;
+//      }
+//    }
+//  }
+//}
+
 std::vector<CandidateTuple>
 getOperandCandidateTuples(MLIRContext &ctx, RegisteredOperationName opName,
                           std::vector<CandidatePtr> &operandCandidates) {
@@ -111,9 +210,12 @@ getOperandCandidateTuples(MLIRContext &ctx, RegisteredOperationName opName,
   int numRegions = getRequiredNumRegions(op);
   std::vector<std::shared_ptr<Region>> regionCandidates = getRegions(builder);
 
+//  return foo[numOperands-1][numAttributes-1][numRegions-1]()
+
   if (numOperands == 1 && numAttributes == 1 && numRegions == 0)
-    return getCartesianProduct110(operandCandidates, attributeCandidates,
-                                  regionCandidates);
+    return getCartesianProduct<1, 1, 0> (operandCandidates, attributeCandidates, regionCandidates);
+    //return getCartesianProduct110(operandCandidates, attributeCandidates,
+    //                              regionCandidates);
   if (numOperands == 1 && numAttributes == 1 && numRegions == 1)
     return getCartesianProduct111(operandCandidates, attributeCandidates,
                                   regionCandidates);
