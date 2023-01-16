90810534555b4fa0a9df978b577cdd0ef89d3ae7
Alexander Brauckmann <a.brauckmann@gmail.com>
2023-01-16 20:01:20 +0000

    Remove RelWithDebInfo config for now

--- build_tools/build_irSynth.sh
+++ build_tools/build_irSynth.sh
@@ -22,7 +22,6 @@ cmake .. -GNinja \
   -DLLVM_ENABLE_LLD=ON \
   -DMLIR_DIR=${PWD}/../mlir-hlo/llvm-build/lib/cmake/mlir \
   -DMHLO_DIR=${PWD}/../mlir-hlo/build/cmake/modules/CMakeFiles \
-  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DCMAKE_C_COMPILER=clang \
   -DCMAKE_CXX_COMPILER=clang++ \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
