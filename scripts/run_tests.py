dec7415e693ecea11fa7b617e3d32c8eb796b2f5
Alexander Brauckmann <a.brauckmann@gmail.com>
2023-02-09 07:31:53 +0000

    [exp] Add distribute and max_num_ops to exp_best mode, make benchmarks named tuples

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,33 +1,42 @@
-import tqdm
-
 import argparse
+import collections
 import json
 import multiprocessing
 import os
 import subprocess
 import time
+
+import tqdm
 import pandas as pd
 
-# fmt: off
-tests = [
-    ('benchmarks/doitgen.mlir', 'doitgen', ['mhlo.dot_general']),
-    ('testfiles/correlation_1.mlir', 'correlation_1', ['chlo.broadcast_divide', 'mhlo.reduce']),
-#    ('test/correlation_3.mlir', 'correlation_3', ['chlo.broadcast_subtract', 'chlo.broadcast_multiply', 'chlo.broadcast_divide']),
-    ('benchmarks/atax.mlir', 'atax', ['mhlo.dot','chlo.broadcast_add','chlo.broadcast_subtract']),
-    ('benchmarks/3mm.mlir', '3mm', ['mhlo.dot']),
-    ('benchmarks/mvt.mlir', 'mvt', ['mhlo.dot', 'chlo.broadcast_add']),
-    ('benchmarks/bicg.mlir', 'bicg', ['mhlo.dot', 'chlo.broadcast_subtract']),
-    ('benchmarks/2mm.mlir', '2mm', ['mhlo.dot', 'chlo.broadcast_multiply', 'chlo.broadcast_add']),
-    ('benchmarks/gemm.mlir', 'gemm', ['chlo.broadcast_add', 'mhlo.dot', 'chlo.broadcast_multiply']),
-#    ('benchmarks/gemver.mlir', 'gemver', []),
-    ('benchmarks/gesummv.mlir', 'gesummv', ['chlo.broadcast_add', 'mhlo.dot', 'chlo.broadcast_multiply']),
+
+Benchmark = collections.namedtuple(
+    'Benchmark', ['file', 'name', 'ops', 'distribute', 'max_num_ops'])
+
+benchmarks = [
+    Benchmark('benchmarks/doitgen.mlir', 'doitgen',
+              ['mhlo.dot_general'], False, 3),
+    Benchmark('testfiles/correlation_1.mlir', 'correlation_1',
+              ['chlo.broadcast_divide', 'mhlo.reduce'], False, 3),
+    Benchmark('benchmarks/atax.mlir', 'atax',
+              ['mhlo.dot', 'chlo.broadcast_add', 'chlo.broadcast_subtract'], True, 3),
+    Benchmark('benchmarks/3mm.mlir', '3mm', ['mhlo.dot'], False, 3),
+    Benchmark('benchmarks/mvt.mlir', 'mvt',
+              ['mhlo.dot', 'chlo.broadcast_add'], False, 3),
+    Benchmark('benchmarks/bicg.mlir', 'bicg',
+              ['mhlo.dot', 'chlo.broadcast_subtract'], True, 3),
+    Benchmark('benchmarks/2mm.mlir', '2mm',
+              ['mhlo.dot', 'chlo.broadcast_multiply', 'chlo.broadcast_add'], False, 3),
+    Benchmark('benchmarks/gemm.mlir', 'gemm',
+              ['chlo.broadcast_add', 'mhlo.dot', 'chlo.broadcast_multiply'], True, 3),
+    Benchmark('benchmarks/gesummv.mlir', 'gesummv',
+              ['chlo.broadcast_add', 'mhlo.dot', 'chlo.broadcast_multiply'], True, 3),
 ]
-# fmt: on
 
-# Get script directory
-script_dir = os.path.dirname(os.path.realpath(__file__))
 timeout = 300
-
+script_dir = os.path.dirname(os.path.realpath(__file__))
+cpu_count = multiprocessing.cpu_count()
+program = os.path.join(script_dir, '../build/bin/synthesizer')
 
 def run_program(x):
     start = time.time()
@@ -38,70 +47,90 @@ def run_program(x):
     return p.stdout.decode('utf-8'), end-start, p.returncode
 
 
-def run_tests(tests, prune_eq_configs=[True, False],
+def run_benchmark(benchmark, prune_equivalent_candidates, ops, distribute, max_num_ops):
+    print('Running benchmark: ' + benchmark.file)
+
+    filename = os.path.join(script_dir, '../' + benchmark.file)
+    args = ['--num-threads=%d' % cpu_count,
+            '--max-num-ops=%d' % max_num_ops,
+            '--print-stats']
+    if prune_equivalent_candidates:
+        args += ['--ignore-equivalent-candidates']
+
+    if ops == 'ground_truth':
+        args += ['--ops=' + ','.join(benchmark.ops)]
+    elif ops == 'heuristic':
+        args += ['--guide']
+
+    if distribute:
+        args += ['--distribute']
+
+    # Run
+    out, synth_time, returncode = run_program(
+        ['timeout', str(timeout)] + [program, filename] + args)
+
+    # Record stats
+    stats = {}
+    stats['benchmark_file'] = benchmark.file.split('.')[0]
+    stats['benchmark'] = benchmark.name
+    stats['prune_equivalent_candidates'] = prune_equivalent_candidates
+    stats['operations'] = ops
+    stats['distribute'] = distribute
+    stats['cmd'] = ' '.join([program, filename] + args)
+
+    stats['status'] = returncode
+
+    if returncode == 0:
+        statsStr = out.split('JSON: ')[1].split('\n')[0]
+        stats.update(json.loads(statsStr))
+
+        stats['synth_time'] = synth_time
+    else:
+        print('Synthesis failed')
+
+        # Timeout
+        if returncode == 124:
+            stats['synth_time'] = synth_time
+
+    return stats
+
+
+def run_benchmarks_all(benchmarks, prune_eq_configs=[True, False],
               ops_configs=['ground_truth', 'heuristic', 'all'],
               distribute_configs=[True, False]):
-    cpu_count = multiprocessing.cpu_count()
-    print('Running experiments on %d cores' % cpu_count)
 
-    program = os.path.join(script_dir, '../build/bin/synthesizer')
-
-    stats_all = []
-    for test in tqdm.tqdm(tests):
-        test_file, test_name, allowed_ops = test
-        print('Running test: ' + test_file)
-
-        test = os.path.join(script_dir, '../' + test_file)
-
+    configs = []
+    for benchmark in benchmarks:
         for prune_equivalent_candidates in prune_eq_configs:
             for ops in ops_configs:
                 for distribute in distribute_configs:
-                    args = ['--num-threads=%d' % cpu_count,
-                            '--print-stats',
-                            '--max-num-ops=6']
-                    if prune_equivalent_candidates:
-                        args += ['--ignore-equivalent-candidates']
+                    configs.append((benchmark, prune_equivalent_candidates, ops, distribute, 6))
 
-                    if ops == 'ground_truth':
-                        args += ['--ops=' + ','.join(allowed_ops)]
-                    elif ops == 'heuristic':
-                        args += ['--guide']
+    stats_all = []
+    for config in tqdm.tqdm(configs):
+        stats = run_benchmark(*config)
+        print(stats)
 
-                    if distribute:
-                        args += ['--distribute']
+        stats_all.append(stats)
 
-                    # Run
-                    out, synth_time, returncode = run_program(
-                        ['timeout', str(timeout)] + [program, test] + args)
+    with open('/tmp/stats.json', 'w') as f:
+        json.dump(stats_all, f, indent=2)
 
-                    # Record stats
-                    stats = {}
-                    stats['test_file'] = test_file.split('.')[0]
-                    stats['benchmark'] = test_name
-                    stats['prune_equivalent_candidates'] = prune_equivalent_candidates
-                    stats['operations'] = ops
-                    stats['distribute'] = distribute
-                    stats['cmd'] = ' '.join([program, test] + args)
 
-                    stats['status'] = returncode
+def run_benchmarks_best(benchmarks):
+    stats_all = []
+    for benchmark in tqdm.tqdm(benchmarks):
+        stats = run_benchmark(benchmark=benchmark,
+                              prune_equivalent_candidates=True,
+                              ops='ground_truth',
+                              distribute=benchmark.distribute,
+                              max_num_ops=benchmark.max_num_ops)
+        print(stats)
 
-                    if returncode == 0:
-                        statsStr = out.split('JSON: ')[1].split('\n')[0]
-                        stats.update(json.loads(statsStr))
+        stats_all.append(stats)
 
-                        stats['synth_time'] = synth_time
-                    else:
-                        print('Synthesis failed')
-
-                        # Timeout
-                        if returncode == 124:
-                            stats['synth_time'] = synth_time
-
-                    print(stats)
-                    stats_all.append(stats)
-
-                    with open('/tmp/stats.json', 'w') as f:
-                        json.dump(stats_all, f, indent=2)
+    with open('/tmp/stats.json', 'w') as f:
+        json.dump(stats_all, f, indent=2)
 
 
 def plot_results():
@@ -118,18 +147,17 @@ def plot_results():
 
 def main():
     # Parse command line arguments
-    parser = argparse.ArgumentParser(description='Run tests')
+    parser = argparse.ArgumentParser(description='Run benchmarks')
     parser.add_argument('--exp_all', action='store_true',
                         default=False, help='Run all experiments')
-    parser.add_argument('--exp_fast', action='store_true',
-                        default=False, help='Run fast experiments')
+    parser.add_argument('--exp_best', action='store_true',
+                        default=False, help='Run experiments in their best configuration')
     args = parser.parse_args()
 
     if args.exp_all:
-        run_tests(tests)
-    elif args.exp_fast:
-        run_tests(tests, prune_eq_configs=[True], ops_configs=['ground_truth'],
-                  distribute_configs=[False])
+        run_benchmarks_all(benchmarks)
+    elif args.exp_best:
+        run_benchmarks_best(benchmarks)
 
     # Plot results.
     plot_results()
