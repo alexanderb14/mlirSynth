eabec854ac383d562fd1778e665697a3e4b89a5b
Alexander Brauckmann <a.brauckmann@gmail.com>
2023-04-13 17:06:58 +0100

    [test] Add linalg experiments

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -14,13 +14,13 @@ import pandas as pd
 Benchmark = collections.namedtuple(
     'Benchmark', ['file', 'name', 'ops', 'distribute', 'max_num_ops'])
 
-benchmarks = [
+benchmarks_hlo = [
     Benchmark('benchmarks/covariance.mlir', 'covariance',
               ['stablehlo.dot_general', 'stablehlo.transpose', 'chlo.broadcast_divide', 'chlo.broadcast_subtract', 'stablehlo.reduce'], False, 3),
+    Benchmark('benchmarks/correlation.mlir', 'correlation',
+              ['stablehlo.dot_general', 'chlo.broadcast_divide', 'chlo.broadcast_subtract'], False, 3),
     Benchmark('benchmarks/doitgen.mlir', 'doitgen',
               ['stablehlo.dot_general'], False, 3),
-    Benchmark('testfiles/correlation_1.mlir', 'correlation_1',
-              ['chlo.broadcast_divide', 'stablehlo.reduce'], False, 3),
     Benchmark('benchmarks/atax.mlir', 'atax',
               ['stablehlo.dot', 'chlo.broadcast_add', 'chlo.broadcast_subtract'], True, 3),
     Benchmark('benchmarks/3mm.mlir', '3mm', ['stablehlo.dot'], False, 3),
@@ -42,6 +42,23 @@ benchmarks = [
               ['stablehlo.transpose', 'stablehlo.dot', 'chlo.broadcast_multiply', 'stablehlo.add', 'stablehlo.select'], True, 5),
 ]
 
+benchmarks_linalg = [
+    Benchmark('benchmarks/2mm.mlir', '2mm',
+              ['linalg.matmul'], True, 1),
+    Benchmark('benchmarks/3mm.mlir', '3mm',
+              ['linalg.matvec'], True, 1),
+    Benchmark('benchmarks/bicg.mlir', 'bicg',
+              ['linalg.matvec'], True, 1),
+    # Benchmark('benchmarks/from_mlt/gemm.mlir', 'gemm',
+    #           ['linalg.matmul', 'linalg.matvec'], True, 1),
+    # Benchmark('benchmarks/gemver.mlir', 'gemver',
+    #           ['linalg.matmul', 'linalg.matvec'], True, 1),
+    Benchmark('benchmarks/gesummv.mlir', 'gesummv',
+              ['linalg.matvec'], True, 1),
+    Benchmark('benchmarks/mvt.mlir', 'mvt',
+              ['linalg.matvec'], True, 1),
+]
+
 timeout = 600
 script_dir = os.path.dirname(os.path.realpath(__file__))
 res_dir = '/tmp/exp_results'
@@ -58,11 +75,12 @@ def run_program(x):
     return p.stdout.decode('utf-8'), end-start, p.returncode
 
 
-def run_benchmark(benchmark, prune_equivalent_candidates, ops, distribute, max_num_ops):
+def run_benchmark(dialect, benchmark, prune_equivalent_candidates, ops, distribute, max_num_ops):
     print('Running benchmark: ' + benchmark.file)
 
     filename = os.path.join(script_dir, '../' + benchmark.file)
-    args = ['--num-threads=%d' % cpu_count,
+    args = ['--target-dialect=%s' % dialect,
+            '--num-threads=%d' % cpu_count,
             '--max-num-ops=%d' % max_num_ops,
             '--print-stats']
     if prune_equivalent_candidates:
@@ -111,7 +129,7 @@ def run_benchmark(benchmark, prune_equivalent_candidates, ops, distribute, max_n
     return stats
 
 
-def run_benchmarks_all(benchmarks, prune_eq_configs=[True, False],
+def run_benchmarks_all(benchmarks, dialect, prune_eq_configs=[True, False],
                        ops_configs=['ground_truth', 'heuristic', 'all'],
                        distribute_configs=[True, False]):
 
@@ -121,7 +139,7 @@ def run_benchmarks_all(benchmarks, prune_eq_configs=[True, False],
             for ops in ops_configs:
                 for distribute in distribute_configs:
                     configs.append(
-                        (benchmark, prune_equivalent_candidates, ops, distribute, 6))
+                        (dialect, benchmark, prune_equivalent_candidates, ops, distribute, 6))
 
     stats_all = []
     for config in tqdm.tqdm(configs):
@@ -134,10 +152,11 @@ def run_benchmarks_all(benchmarks, prune_eq_configs=[True, False],
             json.dump(stats_all, f, indent=2)
 
 
-def run_benchmarks_best(benchmarks):
+def run_benchmarks_best(benchmarks, dialect):
     stats_all = []
     for benchmark in tqdm.tqdm(benchmarks):
         stats = run_benchmark(benchmark=benchmark,
+                              dialect=dialect,
                               prune_equivalent_candidates=True,
                               ops='ground_truth',
                               distribute=benchmark.distribute,
@@ -169,16 +188,25 @@ def main():
                         default=False, help='Run all experiments')
     parser.add_argument('--exp_best', action='store_true',
                         default=False, help='Run experiments in their best configuration')
+    parser.add_argument('--dialect', type=str, help='Dialect to use')
     args = parser.parse_args()
 
     if os.path.exists(res_dir):
         shutil.rmtree(res_dir)
     os.mkdir(res_dir)
 
+    benchmarks = None
+    if args.dialect == 'hlo':
+        benchmarks = benchmarks_hlo
+    elif args.dialect == 'linalg':
+        benchmarks = benchmarks_linalg
+    else:
+        raise Exception('Unknown dialect')
+
     if args.exp_all:
-        run_benchmarks_all(benchmarks)
+        run_benchmarks_all(benchmarks, args.dialect)
     elif args.exp_best:
-        run_benchmarks_best(benchmarks)
+        run_benchmarks_best(benchmarks, args.dialect)
 
     # Plot results.
     plot_results()
