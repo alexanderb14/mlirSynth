6baa9329b1bb9587d678661347cc77412a62d365
Alexander Brauckmann <a.brauckmann@gmail.com>
2023-05-24 09:49:02 +0100

    [transform] Add transform to fold to_tensor and to_memref

--- irSynth/transforms/FoldToTensorToMemrefPairPass.h
+++ irSynth/transforms/FoldToTensorToMemrefPairPass.h
@@ -4,14 +4,17 @@
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/Pass/Pass.h"
 
-std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>> createFoldToTensorToMemrefPairPass();
+std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>>
+createFoldToTensorToMemrefPairPass();
 
 struct FoldToTensorToMemrefPairPass
     : public mlir::PassWrapper<FoldToTensorToMemrefPairPass,
                                mlir::OperationPass<mlir::ModuleOp>> {
   MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(FoldToTensorToMemrefPairPass)
 
-  llvm::StringRef getArgument() const override { return "fold-totensor-tomemref-pairs"; }
+  llvm::StringRef getArgument() const override {
+    return "fold-totensor-tomemref-pairs";
+  }
   llvm::StringRef getDescription() const override {
     return "Folds to_tensor and to_memref pairs.";
   }
@@ -19,7 +22,9 @@ struct FoldToTensorToMemrefPairPass
 };
 
 namespace mlir {
-inline void registerFoldToTensorToMemrefPairPass() { PassRegistration<FoldToTensorToMemrefPairPass>(); }
+inline void registerFoldToTensorToMemrefPairPass() {
+  PassRegistration<FoldToTensorToMemrefPairPass>();
+}
 } // namespace mlir
 
 #endif // IRSYNTH_TRANSFORMS_FOLDTOTENSORTOMENREFPAIRPASS_H
