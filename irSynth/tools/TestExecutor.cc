b1a98f3a20d729c65c1b70d5ec40b3588af01912
Alexander Brauckmann <a.brauckmann@gmail.com>
2023-10-16 14:36:29 +0200

    [val] Add initial check validator

--- irSynth/tools/TestExecutor.cc
+++ irSynth/tools/TestExecutor.cc
@@ -3,7 +3,13 @@
 #include "execution/ArrayUtils.h"
 #include "execution/Executor.h"
 #include "execution/Lowering.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Location.h"
 #include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
 #include "mlir/Tools/ParseUtilities.h"
 #include "synthesis/Synthesizer.h"
@@ -116,6 +122,155 @@ bool testValidate(func::FuncOp lhsFunction, func::FuncOp rhsFunction,
   return areArraysEqual(refOut, rhsOut, targetShape);
 }
 
+bool checkValidate(func::FuncOp lhsFunction, func::FuncOp rhsFunction,
+                  bool printArgsAndResults = false, bool printResults = false) {
+  auto *ctx = lhsFunction->getContext();
+  auto pm = std::make_shared<mlir::PassManager>(ctx);
+
+  auto builder = OpBuilder(ctx);
+
+  OwningOpRef<ModuleOp> module(ModuleOp::create(UnknownLoc::get(ctx)));
+  auto &moduleBlock = module->getRegion().getBlocks().front();
+  builder.setInsertionPoint(&moduleBlock, moduleBlock.begin());
+
+  // Rename and copy over functions.
+  auto lhsFunctionClone = lhsFunction.clone();
+  lhsFunctionClone.setSymName("lhs");
+  module->push_back(lhsFunctionClone);
+
+  auto rhsFunctionClone = rhsFunction.clone();
+  rhsFunctionClone.setSymName("rhs");
+  module->push_back(rhsFunctionClone);
+
+  // Create a main function.
+  auto mainFunc = builder.create<func::FuncOp>(UnknownLoc::get(ctx), "main",
+                                           builder.getFunctionType({}, {}));
+
+  auto &bodyBlock = *mainFunc.addEntryBlock();
+  builder.setInsertionPoint(&bodyBlock, bodyBlock.begin());
+
+  // Create operands.
+  SmallVector<mlir::Value> operands;
+  for (auto arg : lhsFunction.getArguments()) {
+    if (arg.getType().isa<ShapedType>()) {
+      auto memreftype = arg.getType().cast<MemRefType>();
+      auto memref = builder.create<memref::AllocOp>(UnknownLoc::get(ctx), memreftype);
+      operands.push_back(memref);
+
+      auto afterLastOperand = builder.saveInsertionPoint();
+
+      SmallVector<mlir::Value> indices;
+      auto argShape = arg.getType().cast<ShapedType>().getShape();
+      for (auto dimSize : argShape) {
+        // Create for op.
+        auto forOp = builder.create<scf::ForOp>(
+            UnknownLoc::get(ctx),
+            builder.create<arith::ConstantIndexOp>(UnknownLoc::get(ctx), 0),
+            builder.create<arith::ConstantIndexOp>(UnknownLoc::get(ctx), dimSize),
+            builder.create<arith::ConstantIndexOp>(UnknownLoc::get(ctx), 1));
+
+        indices.push_back(forOp.getInductionVar());
+
+        // Set insertion point inside body for next loop to be inserted.
+        builder.setInsertionPointToStart(forOp.getBody());
+
+        // If we are in the innermost loop, create the declaration.
+        if (indices.size() == argShape.size()) {
+          // Create decl.
+          SmallVector<mlir::Value> operands = {};
+          auto declOp = builder.create<func::CallOp>(UnknownLoc::get(ctx), 
+                                       builder.getF64Type(),
+                                       "cbmc_declare", operands);
+
+          // Create store.
+          builder.create<memref::StoreOp>(UnknownLoc::get(ctx),
+                                          declOp->getResult(0),
+                                          memref.getMemref(), indices);
+        }
+      }
+
+      builder.restoreInsertionPoint(afterLastOperand);
+
+    } else if (arg.getType().isa<FloatType>()) {
+      operands.push_back(builder.create<arith::ConstantOp>(
+          UnknownLoc::get(ctx), builder.getF64Type(),
+          builder.getF64FloatAttr(0.0f)));
+    } else {
+      llvm::outs() << "Type: " << arg.getType() << "\n";
+      assert(false && "Unsupported type");
+    }
+  }
+
+  // Call functions on the created operands.
+  auto lhsCallOp = builder.create<func::CallOp>(
+      UnknownLoc::get(ctx), lhsFunctionClone.getResultTypes(),
+      lhsFunctionClone.getSymName(), operands);
+  auto rhsCallOp = builder.create<func::CallOp>(
+      UnknownLoc::get(ctx), rhsFunctionClone.getResultTypes(),
+      rhsFunctionClone.getSymName(), operands);
+
+  auto afterLastCallOp = builder.saveInsertionPoint();
+
+  // Assert element-wise equality.
+  SmallVector<mlir::Value> indices;
+  auto shape = lhsFunction.getResultTypes()[0].cast<ShapedType>().getShape();
+  for (auto dimSize : shape) {
+    // Create for op.
+    auto forOp = builder.create<scf::ForOp>(
+        UnknownLoc::get(ctx),
+        builder.create<arith::ConstantIndexOp>(UnknownLoc::get(ctx), 0),
+        builder.create<arith::ConstantIndexOp>(UnknownLoc::get(ctx), dimSize),
+        builder.create<arith::ConstantIndexOp>(UnknownLoc::get(ctx), 1));
+
+    indices.push_back(forOp.getInductionVar());
+
+    // Set insertion point inside body for next loop to be inserted.
+    builder.setInsertionPointToStart(forOp.getBody());
+
+    // If we are in the innermost loop, create the equality check.
+    if (indices.size() == shape.size()) {
+      // Load from memrefs.
+      auto lhsMemref = lhsCallOp.getResult(0);
+      mlir::Type lhsType =
+          lhsMemref.getType().cast<MemRefType>().getElementType();
+      auto lhsLoad = builder.create<memref::LoadOp>(
+          UnknownLoc::get(ctx), lhsType, lhsMemref, indices);
+
+      auto rhsMemref = rhsCallOp.getResult(0);
+      mlir::Type rhsType =
+          rhsMemref.getType().cast<MemRefType>().getElementType();
+      auto rhsLoad = builder.create<memref::LoadOp>(
+          UnknownLoc::get(ctx), rhsType, rhsMemref, indices);
+
+      // Create check.
+      SmallVector<mlir::Value> checkOperands = {lhsLoad, rhsLoad};
+      builder.create<func::CallOp>(UnknownLoc::get(ctx), 
+                                   builder.getF64Type(),
+                                   "cbmc_assert", checkOperands);
+    }
+  }
+
+  // Must have the check function declaration.
+  builder.setInsertionPoint(&moduleBlock, moduleBlock.begin());
+  func::FuncOp cbmcAssertFwdDecl = builder.create<func::FuncOp>(
+      UnknownLoc::get(ctx), "cbmc_assert",
+      mlir::FunctionType::get(ctx, {builder.getF64Type(), builder.getF64Type()},
+                              {builder.getF64Type()}));
+  cbmcAssertFwdDecl.setPrivate();
+
+  func::FuncOp cbmcDeclareFwdDecl = builder.create<func::FuncOp>(
+      UnknownLoc::get(ctx), "cbmc_declare",
+      mlir::FunctionType::get(ctx, {}, {builder.getF64Type()}));
+  cbmcDeclareFwdDecl.setPrivate();
+
+  // Must have a return op.
+  builder.restoreInsertionPoint(afterLastCallOp);
+  builder.create<func::ReturnOp>(UnknownLoc::get(ctx));
+  module->dump();
+
+  return true;
+}
+
 int main(int argc, char **argv) {
   // Parse command line arguments.
   cl::opt<std::string> inputFilename(cl::Positional, cl::desc("<input file>"),
@@ -182,6 +337,8 @@ int main(int argc, char **argv) {
     lowered->dump();
 
     bool equiv = testValidate(originalFunction, lowered,
+                              printArgsAndResults, printResults);
+    checkValidate(originalFunction, lowered,
                               printArgsAndResults, printResults);
 
     // Print the results.
